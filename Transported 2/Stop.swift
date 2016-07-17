9df413694e454f70ef31c086ba32a2b207715215
Luke Stringer <luke.stringer@3squared.com>
2016-07-17 19:41:30 +0100

    Add stop model and MKAnnotation conformance

---
+++ Transported 2/Stop.swift
@@ -0,0 +1,40 @@
+//
+//  Stop.swift
+//  Transported 2
+//
+//  Created by Luke Stringer on 17/07/2016.
+//  Copyright © 2016 Luke Stringer. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+import MapKit
+
+enum StopType {
+	case Bus
+	case Tram
+}
+
+struct Stop {
+	let location: CLLocation
+	let type: StopType
+	let title: String
+	let subtitle: String?
+	
+	func annotation() -> Annotation {
+		return Annotation(coordinate: self.location.coordinate, title: self.title, subtitle: self.subtitle)
+	}
+	
+}
+
+class Annotation: NSObject, MKAnnotation {
+	let coordinate: CLLocationCoordinate2D
+	let title: String?
+	let subtitle: String?
+	
+	init(coordinate: CLLocationCoordinate2D, title: String?, subtitle: String?) {
+		self.coordinate = coordinate
+		self.title = title
+		self.subtitle = subtitle
+	}
+}
