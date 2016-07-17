f8fe1c4738291e24ee3923326b7cbd862828a53e
Luke Stringer <luke.stringer@3squared.com>
2016-07-17 13:06:05 +0100

    Add initial project structure

---
+++ Transported 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Transported 2
+//
+//  Created by Luke Stringer on 17/07/2016.
+//  Copyright © 2016 Luke Stringer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
